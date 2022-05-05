00c560341ffe95fcfd937e6fda51a9b3d2f5de4a
ValdoR <valdesche03@gmail.com>
2022-05-05 15:05:52 +0200

    Add Intentional failed test

---
+++ allure-testng-example/src/test/java/my/company/SimpleTest.java
@@ -0,0 +1,23 @@
+package my.company;
+
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+/**
+ * @author baev (Dmitry Baev)
+ */
+public class SimpleTest {
+
+    @Test(description = "Sample method HTML <b>description :)</b>")
+    public void simpleTestOne() {
+        step("step 1");
+        step("step 2");
+    }
+    @Test
+    public void simpleTestTwo() {
+        step("step 1");
+        step("step 2");
+    }
+}
+
